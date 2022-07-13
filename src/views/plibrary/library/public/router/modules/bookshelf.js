246aefe7a1e6a1fc5774250fe9425902018c8e21
陈赞 <982481700@qq.com>
2022-07-13 23:23:47 +0800

    左侧菜单导航

---
+++ src/views/plibrary/library/public/router/modules/bookshelf.js
@@ -0,0 +1,24 @@
+
+const category = {
+  path: 'categories',
+  meta: { title: '类目', icon: 'Basic-Configuration', noCache: true },
+  redirect: '/unit-evc/baseDeploy/menuManager',
+  alwaysShow: true,
+  component: () => import('@/views/plibrary/library/category/page/index'),
+  children: [
+    {
+      code: 'menuManager',
+      path: 'menuManager',
+      meta: { title: '菜单管理', noCache: true },
+      component: () => import('@/views/IntelligentControl/baseDeploy/page/subpage/menuManager')
+    },
+    {
+      code: 'unit-evcDataInterface',
+      path: 'dataInterface',
+      meta: { title: '数据接口', noCache: true },
+      component: () => import('@/views/IntelligentControl/baseDeploy/page/subpage/dataInterface')
+    }
+  ]
+}
+
+export default category
